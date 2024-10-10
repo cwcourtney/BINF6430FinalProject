e51955d0070eba1d17b613269f848abb00c6319f
cwcourtney <wong.courtney@gmail.com>
2024-10-10 15:57:28 -0400

    Use curl instead offasterq-dump  for speed and gzip

--- get_fastq.sh
+++ get_fastq.sh
@@ -1,40 +1,31 @@
 #!/bin/bash
 
-# Usage: sbatch get_fastq <SampleList>
-    # <SampleList> is a csv with two columns (no header): SRR, Sample Name
+# Usage: sbatch get_fastq.sh <SampleList>
+    # <SampleList> is a csv with 3 columns (no header): SRR, Sample Name, ENA link to fastq
 
 #SBATCH --job-name=get_fastq
-#SBATCH --partition=express
+#SBATCH --partition=short
 #SBATCH -N 1
-#SBATCH -c 8
-#SBATCH --mem 32G
-#SBATCH -t 1:00:00
+#SBATCH -c 32
+#SBATCH --mem 64G
+#SBATCH -t 8:00:00
 #SBATCH --mail-type=END,FAIL
 #SBATCH --out=logs/%x_%j.log
 #SBATCH --error=logs/%x_%j.err
 
-# Modules
-module load sratoolkit
-
 # Directories
 OUTDIR=/scratch/${USER}/data
 mkdir -p $OUTDIR
-TMP=/scratch/${USER}/tmp
-mkdir -p $TMP
+
+SAMPLES=$(realpath "$1")
 
 # Iterate through each SRR of each sample
-for sample in $(awk -F, '{ print $2 }' $1 | uniq) # Fetal1, Fetal2, ..., Adult2, Adult3
+for sample in $(awk -F, '{ print $2 }' "$SAMPLES" | uniq) # Fetal1, Fetal2, ..., Adult2, Adult3
 do
     echo "$(date) Processing sample: $sample"
     mkdir -p $OUTDIR/$sample
-    for srr in $(awk -v sname="$sample" -F, '$2 == sname { print $1 }' $1) # SRR...
-    do
-        echo "$(date) Getting FASTQ for run: $srr"
-        fasterq-dump $srr \
-        --outdir $OUTDIR/$sample \
-        --temp $TMP \
-        --threads $SLURM_CPUS_PER_TASK \
-        --split-files \
-        --details
-    done
+    pushd $OUTDIR/$sample
+    awk -v sname="$sample" -F, '$2 == sname { print $3 }' "$SAMPLES" | \
+    xargs curl --parallel -O
+    popd
 done
