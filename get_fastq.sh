45b355321da1aff9f54f52e1b1bb43c8bc085a08
cwcourtney <wong.courtney@gmail.com>
2024-10-10 14:23:48 -0400

    Updated get_fastq to accept csv of SRR,SampleName as input

--- get_fastq.sh
+++ get_fastq.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Usage: sbatch get_fastq <SampleList>
+    # <SampleList> is a csv with two columns (no header): SRR, Sample Name
+
 #SBATCH --job-name=get_fastq
 #SBATCH --partition=express
 #SBATCH -N 1
@@ -18,14 +22,19 @@ mkdir -p $OUTDIR
 TMP=/scratch/${USER}/tmp
 mkdir -p $TMP
 
-# Extract SR Run IDs (col 5) where Title (col 2) contains 'heart', omit header row
-for sample in $(awk -F, 'NR >1 && $2 ~ /heart/ { print $5  }' SampleList.csv)
+# Iterate through each SRR of each sample
+for sample in $(awk -F, '{ print $2 }' $1 | uniq) # Fetal1, Fetal2, ..., Adult2, Adult3
 do
-    echo "$(date) Getting FASTQ from: $sample"
-    fasterq-dump $sample \
-    --outdir $OUTDIR \
-    --temp $TMP \
-    --threads $SLURM_CPUS_PER_TASK \
-    --progress \
-    --split-files
+    echo "$(date) Processing sample: $sample"
+    mkdir -p $OUTDIR/$sample
+    for srr in $(awk -v sname="$sample" -F, '$2 == sname { print $1 }' $1) # SRR...
+    do
+        echo "$(date) Getting FASTQ for run: $srr"
+        fasterq-dump $srr \
+        --outdir $OUTDIR/$sample \
+        --temp $TMP \
+        --threads $SLURM_CPUS_PER_TASK \
+        --split-files \
+        --details
+    done
 done
